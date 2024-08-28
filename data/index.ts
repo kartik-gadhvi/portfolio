5deb55d3c29790beac75623bacd6883c15db6b56
kartik-gadhvi <kartik.suru@st.ovgu.de>
2024-08-28 03:38:57 +0200

    dfdf

---
+++ data/index.ts
@@ -0,0 +1,217 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building an AI SaaS website",
+    description: "What am I up to?",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Would you like to work on a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Website of the HCAi Symposium 2024 OVGU",
+    des: "Developed responsive website for HCAi Symposium 2024 at OVGU Magdeburg, featuring event info, registration.",
+    img: "Hcai.png",
+    iconLists: [ "/html-icon.svg",  "/three.svg","/bootstrap-5-logo-icon.svg","/javascript-programming-language-icon.svg" ],
+    link: "https://kartik-gadhvi.github.io/Hcai-Symposium2024/",
+  },
+  // {
+  //   id: 2,
+  //   title: "Yoom - Video Conferencing App",
+  //   des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
+  //   img: "/p2.svg",
+  //   iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+  //   link: "/ui.yoom.com",
+  // },
+  // {
+  //   id: 3,
+  //   title: "AI Image SaaS - Canva Application",
+  //   des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
+  //   img: "/p3.svg",
+  //   iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+  //   link: "/ui.aiimg.com",
+  // },
+  {
+    id: 4,
+    title: "Animated Apple Iphone 3D Website",
+    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+    img: "/p4.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "https://kartik-gadhvi.github.io/iPhone15-website-clone/",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Mr. Suru excelled in both frontend and backend development, proficiently utilizing HTML, CSS, JavaScript, Node.js, and Express.js. His mastery of programming languages, including JavaScript, and python, coupled with expertise in Git and frameworks like React.js, significantly contributed to project success.",
+    name: "Manan Chauhan",
+    title: "Founder and CEO WZero Infotech",
+  },
+  {
+    quote:
+      "Mr. Suru excelled in both frontend and backend development, proficiently utilizing HTML, CSS, JavaScript, Node.js, and Express.js. His mastery of programming languages, including JavaScript, and python, coupled with expertise in Git and frameworks like React.js, significantly contributed to project success.",
+    name: "Manan Chauhan",
+    title: "Founder and CEO WZero Infotech",
+  },
+  {
+    quote:
+      "Mr. Suru excelled in both frontend and backend development, proficiently utilizing HTML, CSS, JavaScript, Node.js, and Express.js. His mastery of programming languages, including JavaScript, and python, coupled with expertise in Git and frameworks like React.js, significantly contributed to project success.",
+    name: "Manan Chauhan",
+    title: "Founder and CEO WZero Infotech",
+  },
+  {
+    quote:
+      "Mr. Suru excelled in both frontend and backend development, proficiently utilizing HTML, CSS, JavaScript, Node.js, and Express.js. His mastery of programming languages, including JavaScript, and python, coupled with expertise in Git and frameworks like React.js, significantly contributed to project success.",
+    name: "Manan Chauhan",
+    title: "Founder and CEO WZero Infotech",
+  },
+  {
+    quote:
+      "Mr. Suru excelled in both frontend and backend development, proficiently utilizing HTML, CSS, JavaScript, Node.js, and Express.js. His mastery of programming languages, including JavaScript, and python, coupled with expertise in Git and frameworks like React.js, significantly contributed to project success.",
+    name: "Manan Chauhan",
+    title: "Founder and CEO WZero Infotech",
+  },
+];
+
+export const companies = [
+  // {
+  //   id: 1,
+  //   name: "cloudinary",
+  //   img: "/cloud.svg",
+  //   nameImg: "/cloudName.svg",
+  // },
+  // {
+  //   id: 2,
+  //   name: "appwrite",
+  //   img: "/app.svg",
+  //   nameImg: "/appName.svg",
+  // },
+  // {
+  //   id: 3,
+  //   name: "HOSTINGER",
+  //   img: "/host.svg",
+  //   nameImg: "/hostName.svg",
+  // },
+  // {
+  //   id: 4,
+  //   name: "stream",
+  //   img: "/s.svg",
+  //   nameImg: "/streamName.svg",
+  // },
+  // {
+  //   id: 5,
+  //   name: "docker.",
+  //   img: "/dock.svg",
+  //   nameImg: "/dockerName.svg",
+  // },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Full Stack Developer",
+    desc: "Engineered full-stack web applications, leveraging React.js frontends and robust backend infrastructures to deliver seamless, interactive user experiences",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Building a SaaS AI website",
+    desc: "Architecting a cutting-edge SaaS platform - where AI meets limitless possibilities.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Freelance website projects",
+    desc: "Built and deployed many projects for local clients back in India.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Lead Developer",
+    desc: "Developed and maintained the website of the HCAi Symposium 2024 OVGU.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/kartik-gadhvi",
+    alt: "GitHub"
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/kartik-suru-004b47218/",
+    alt: "LinkedIn"
+  },
+];
